ec27a259baece433c4cfaaa8bff65166cba4f924
Akhil Gandu <akhilhello@gmail.com>
2020-09-13 17:59:53 -0700

    added configuration reader class

---
+++ Config.java
@@ -0,0 +1,8 @@
+import java.util.*; 
+
+public class Config {
+
+    public void say_hi() {
+        System.out.println("Hello"); 
+    }
+}
--- WebServer.java
+++ WebServer.java
@@ -4,5 +4,8 @@ public class WebServer {
 
     public static void main(String... args) {
         System.out.println("hello wrld"); 
+        Config a = new Config(); 
+
+        a.say_hi(); 
     }
 }

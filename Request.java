238ef54bbc24d8a007cea76532d4ea2472b99807
kkennedy41 <56007720+kkennedy41@users.noreply.github.com>
2020-09-17 13:34:41 -0700

    uri resolves started

--- Request.java
+++ Request.java
@@ -1,3 +1,5 @@
+
+
 import java.util.*; 
 import java.io.*; 
 import java.net.*; 
@@ -86,4 +88,40 @@ public class Request {
             }
         }
     }
+
+  // create method Boolean is_uri_alised(), if it is, also modify the path, and resolve the full URI
+  public Boolean is_uri_aliased(){
+    if(Headers.containsKey("Alias")){
+
+      return true;
+    }
+
+    return false;
+  }
+
+
+// create method BOolean is_script_alised(), if it is, modify the path, and resolve the FULL URI, also,
+// we can set a variable, saying it's a script
+  public Boolean is_script_aliased(){
+    if(Headers.containsKey("ScriptAlias")){
+      return true;
+    }
+    else{
+      resolve_document_root(path);
+    }
+    return false;
+  }
+
+// create method void resolve_document_root(String doc_root)
+  public void resolve_document_root(String doc_root){
+   
+  }
+
+// create method void is_file(), if it is a file, we're good to go, otherwise, check directory index
+  public void is_file(){
+
+  }
+
 }
+
+

d3a1129719c4b4dac925772aca7720cb60ae505b
kkennedy41 <56007720+kkennedy41@users.noreply.github.com>
2020-09-18 14:15:01 -0700

    trying to handle parsing the SHA key

--- Authentication.java
+++ Authentication.java
@@ -1,9 +1,8 @@
-
-
+import java.io.*;
 
 public class Authentication {
   
-  private String path;
+  private String pwFilePath;
   private String authType;
   private String authName;
   private String requireArg;
@@ -12,9 +11,30 @@ public class Authentication {
 
   }
 
+  public void parse_htaccess_file(){
 
-  public parsePassword(){
-    
   }
+
+
+  public void parse_pw_file(){
+    BufferedReader pwFile = new BufferedReader(new FileReader(pwFilepath));
+    String data = pwFile.readLine();
+    String user = "";
+    String pwHash = "";
+    char ch;
+    int i = 0;
+
+    while(ch != ':'){
+      ch = data.charAt(i);
+      user = user + ch;
+      i++;
+    }
+
+    for(int j = i+1; j < data.length(); j++){
+      pwHash = pwHash + data.charAt(j);
+    }
+  }
+
+
 
 }
